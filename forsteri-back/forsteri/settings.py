05ddb8a15e4266dc9a92f8c61f318814ff850c8d
palmoisii <kaito.mznf@gmail.com>
2023-07-07 23:28:02 +0900

    タイムアウト時間を30分に修正

--- forsteri-back/forsteri/settings.py
+++ forsteri-back/forsteri/settings.py
@@ -171,7 +171,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     #トークンの時間を5分に設定
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
