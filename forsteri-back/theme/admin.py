db04cfe32115c2c3dd0cdcca014faa1cca3d96cf
palmoisii <kaito.mznf@gmail.com>
2023-07-12 12:31:00 +0900

    投票機能、スタッフビュー作成。票数、id並び替えは未実装。

--- forsteri-back/theme/admin.py
+++ forsteri-back/theme/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Theme
+from .models import Theme,Vote
 
 # Register your models here.
 admin.site.register(Theme)
+admin.site.register(Vote)
--- forsteri-back/theme/views.py
+++ forsteri-back/theme/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
-from .models import Theme
+from .models import Theme,Vote
 from .forms import ThemeForm
 from rest_framework import generics
 
@@ -11,8 +11,7 @@ from .models import Theme
 from .serializers import ThemeSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from rest_framework.permissions import AllowAny
-from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 class CreateTheme(generics.CreateAPIView):
     queryset = Theme.objects.all()
@@ -48,6 +47,7 @@ class ListTheme(APIView):
                     'theme': d.theme,
                     'comment': d.comment,
                     'id': d.id,  # idフィールドを追加
+                    'votes' : d.votes
                 }
                 for d in theme
             ]
@@ -66,11 +66,25 @@ class DetailTheme(APIView):
             res = {
                 'id': theme.id,
                 'theme': theme.theme,
-                'comment': theme.comment
+                'comment': theme.comment,
+                'votes': theme.votes
             }
             return Response(res)
         except:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
 class VoteThemeView(APIView):
-    pass
+    def post(self, request, pk):
+        theme = get_object_or_404(Theme, pk=pk)
+        voter = request.user.email  # ログインユーザーのメールアドレスを取得（仮定）
+
+        # 同一ユーザーからの投票が存在しない場合のみ処理を実行
+        if not Vote.objects.filter(voter=voter).exists():
+            theme.votes += 1  # votesを増やす
+            theme.save()
+
+            vote = Vote.objects.create(theme=theme, voter=voter)  # Voteレコードを作成
+
+            return Response({'message': '投票が成功しました'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': '既に投票済みです'}, status=status.HTTP_400_BAD_REQUEST)
